62c81f58a8e4ce116a0030d1761fac5b26a0858d
Yuki Kurosawa Hotkey <yuki@ksyuki.com>
2025-07-01 11:51:54 +0800

    Move demo.c to info.c

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ rm -rf www
 
 gcc -DGLOBAL_CONFIG_PATH="\"./www.conf\"" -DSITES_DIR_PATH="\"./sites\"" \
 -o www \
-src/www.c src/client.c src/parser.c src/demo.c \
+src/www.c src/client.c src/parser.c src/handlers/info.c \
 -pthread -lssl -lcrypto \
 -lpcre2-8
 
